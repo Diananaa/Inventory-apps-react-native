65bf88b775cac631382e34a74a747418b4a07e97
Diana <d1anasite04@gmail.com>
2023-12-14 11:43:33 +0700

    refactor: api login

--- src/utils/api/auth/index.jsx
+++ src/utils/api/auth/index.jsx
@@ -1,16 +1,6 @@
 import axios from "axios"
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
 
 export const loginApi = async (data) =>
     axios.post('https://mobile.dev.quadrant-si.id/developertest/Login', data)
         .then((res) => res.data)
 
-export const getData = async () => {
-    try {
-        const jsonValue = await AsyncStorage.getItem('auth');
-        return jsonValue != null ? JSON.parse(jsonValue) : null;
-    } catch (e) {
-        // error reading value
-    }
-};
