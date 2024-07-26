26041999c9a1eb643adb6d64cf9ef645d801bc5e
diana.nana <diana.nana@quadrant-si.id>
2024-07-26 10:28:42 +0700

    fix: refactor CustomDrawerContent

--- src/pages/Component/CustomDrawerContent.jsx
+++ src/pages/Component/CustomDrawerContent.jsx
@@ -1,47 +1,50 @@
-// components/CustomDrawerContent.js
 import { DrawerContentScrollView, DrawerItem } from '@react-navigation/drawer';
 import React from 'react';
 import { Image, TouchableOpacity } from 'react-native';
 import { ICArrowRight, ICHome, ICHomeActive, ICMenuBox, ICMenuBoxActive } from '../../assets/mza/icon';
 import IconWithLabel from '../../components/atoms/IconWithLabel';
+import Themes from '../../utils/Themes';
 
 export default function CustomDrawerContent(props) {
     const { navigation } = props;
     const { state } = props;
     const activeRoute = state.routeNames[state.index];
+    const MENU = [
+        {
+            label: 'Home',
+            name: 'Home',
+            icon: ICHome,
+            iconActive: ICHomeActive
+        }, {
+            label: 'Semua Menu',
+            name: 'menu',
+            icon: ICMenuBox,
+            iconActive: ICMenuBoxActive
+        }
+    ]
     return (
         <DrawerContentScrollView {...props}>
-            <TouchableOpacity onPress={() => navigation.closeDrawer()} style={{ width: '100%', height: 50, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <Image source={ICArrowRight} style={{width: 24, height: 24}} />
+            <TouchableOpacity onPress={() => navigation.closeDrawer()} style={{ width: '100%', borderBottomColor:Themes.COLOR.gray, borderBottomWidth:1, height: 50, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <Image source={ICArrowRight} style={{ width: 24, height: 24 }} />
             </TouchableOpacity>
-            <DrawerItem
-                activeBackgroundColor='white'
-                label=""
-                icon={({ focused, color, size }) => (
-                    <IconWithLabel
-                        source={focused ? ICHomeActive : ICHome}
-                        label="Home"
-                        focused={focused}
+            {
+                MENU.map((data, key) => (
+                    <DrawerItem
+                        key={key}
+                        activeBackgroundColor='white'
+                        label=""
+                        icon={({ focused, color, size }) => (
+                            <IconWithLabel
+                                source={focused ? data.iconActive : data.icon}
+                                label={data.label}
+                                focused={focused}
+                            />
+                        )}
+                        focused={activeRoute === data.name}
+                        onPress={() => navigation.navigate(data.name)}
                     />
-                )}
-                focused={activeRoute === 'Home'}
-                onPress={() => navigation.navigate('Home')}
-            />
-
-
-            <DrawerItem
-                label=""
-                activeBackgroundColor='white'
-                icon={({ focused, color, size }) => (
-                    <IconWithLabel
-                        source={focused ? ICMenuBoxActive : ICMenuBox}
-                        label="Semua Menu"
-                        focused={focused}
-                    />
-                )}
-                focused={activeRoute === 'menu'}
-                onPress={() => navigation.navigate('menu')}
-            />
+                ))
+            }
         </DrawerContentScrollView>
     );
 }
